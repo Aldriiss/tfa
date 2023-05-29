2758140b61f46c89f2c76ef98cd6e0f37c00bf1a
Aldriiss <adrienvdb0@gmail.com>
2023-05-29 16:06:20 +0200

    ajout graph personnalisé

--- scripts/app.js
+++ scripts/app.js
@@ -24,25 +24,58 @@ if(burgerButton){
 
 };
 
+// API pour les graph style google
+google.charts.load('current', {'packages':['corechart']});
 
-const color = d3.scaleOrdinal(['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f']);
-const data = [50,50];
-const radius = 125;
-const g = d3.select('svg')
- .append('g')
- .attr('transform', `translate(${radius}, ${radius})`);
+google.charts.setOnLoadCallback(function() {
+  drawChart1();
+  drawChart2();
+});
 
-const arc = d3.arc()
-  .innerRadius(0)
-  .outerRadius(radius);
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
 
-const arcs = d3.pie()(data);
+// premier Graphique
+function drawChart1() {
+  // Donnée du graph1
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Oui Beaucoup', 4],
+    ['Oui', 2],
+    ['Moyennement', 5],
+    ['Non', 3],
+  ]);
 
-g.selectAll('path')
-  .data(arcs)
-  .enter()
-  .append('path')
-  .attr('d', arc)
-  .style('fill', (d, i) => color(i))
-  .style('stroke', 'white')
-  .style('stroke-width', 3);
+  // Titre et taille du graphique
+  var options = {'title':'Votre cuisine vous satisfait-elle ?',
+                 'width':400,
+                 'height':300};
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div--1'));
+  chart.draw(data, options);
+}
+
+// Second Graphique
+function drawChart2() {
+  // Donnée du graph2
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Le temps', 7],
+    ['Le prix', 3],
+    ['Le manque d\'\idées', 5],
+    ['La vaisselles', 2],
+  ]);
+
+  // Titre et taille du graphique
+  var options = {'title':'Quel est le point négatif que vous rencontrez le plus en cuisinant ?',
+                 'width':400,
+                 'height':300};
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div--2'));
+  chart.draw(data, options);
+}
